d04d584025ffcd3c76a151ecaa56b9ed2bb68342
Dinesh <mailtodinesh@gmail.com>
2021-07-15 11:05:48 +0530

    sticky header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React,{useRef, useState, useEffect} from 'react';
 import { toast } from 'react-toastify';
 import './header.css';
 
-function header(props){
+function Header(props){
     function handleLogout(){
         sessionStorage.removeItem("token");
         props.handleToken("");
         toast.success('Logged out succesfully',{autoClose:2000});
     }
 
+    const [isSticky, setSticky] = useState(false);
+    const ref = useRef(null);
+    const handleScroll = () => {
+    if (ref.current) {
+        console.log(ref.current.getBoundingClientRect());
+        //console.log(ref.current.getBoundingClientRect().top);
+      setSticky(ref.current.getBoundingClientRect().top <= 0);
+    }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', () => handleScroll);
+        };
+      }, []);
+
     return(
-        <header className="Header">
-            <div className="heading">
-                <h2 className="head">TODO LIST</h2>
-            </div>
-            <nav className="Nav">
-                <a href="/">ALL</a>
-                <a href="/active">Active</a>
-                <a href="/finished">Finished</a>
-                <button type="submit" onClick={handleLogout}>Logout</button>
-            </nav>
-        </header>        
+        <div className={`sticky-wrapper${isSticky ? ' sticky' : ''}`} ref={ref}>
+            <header className="Header sticky-inner">
+                <div className="heading">
+                    <h4 className="head">TODO LIST</h4>
+                </div>
+                <nav className="Nav">
+                    <a href="/">ALL</a>
+                    <a href="/active">Active</a>
+                    <a href="/finished">Finished</a>
+                    <button type="submit" onClick={handleLogout}>Logout</button>
+                </nav>
+            </header>
+        </div>        
     );
 }
 
-export default header;
+export default Header;
